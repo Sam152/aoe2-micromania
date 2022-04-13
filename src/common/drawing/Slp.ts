cfd6cc3ee02a969fc0d0a12b4b750ea3086e2691
Sam <54m152@gmail.com>
2022-04-13 15:35:03 +0800

    progress

---
+++ src/common/drawing/Slp.ts
@@ -0,0 +1,10 @@
+class Slp {
+    private data: ArrayBuffer;
+
+    constructor(data: ArrayBuffer) {
+        this.data = data;
+    }
+
+
+
+}
--- src/common/drawing/SlxRenderer.ts
+++
@@ -1,5 +0,0 @@
-// Read an SLX file
-// Download the BMP assets
-// Composite shadow/player color/transparency to each asset
-// Split into cardinal directions
-// API for getting image data by: frame #, color, direction
