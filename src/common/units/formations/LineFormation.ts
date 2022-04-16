78464b476457b5eab24edb3c39e8097de36563e5
Sam <54m152@gmail.com>
2022-04-17 01:25:15 +0800

    Working

--- src/common/units/formations/LineFormation.ts
+++ src/common/units/formations/LineFormation.ts
@@ -1,7 +1,6 @@
 import {FormationInterface} from "../../../types";
 import {Vector2} from "three";
 import averageVector from "../../util/averageVector";
-import {circle} from "../../drawing/shapes";
 import rotateAroundOrigin from "../../util/rotateAroundOrigin";
 
 export default class LineFormation implements FormationInterface {
@@ -9,28 +8,43 @@ export default class LineFormation implements FormationInterface {
     distanceBetween = 30;
 
     form(positions: Array<Vector2>, destination: Vector2): Array<Array<Vector2>> {
-
         const startingPoint = averageVector(positions);
 
-        const linedUpDestinations = positions.map((position, index) => {
+        // Line up the units into rows.
+        let newPositions = positions.map((position, index) => {
             return destination.clone().add(new Vector2(index * this.distanceBetween, 0));
         });
-        const offsetFromDestination = averageVector(linedUpDestinations).sub(destination);
 
-        linedUpDestinations.map(linedUpDestination => linedUpDestination.sub(offsetFromDestination));
+        // Move the units to the middle of the destination point.
+        const offsetFromDestination = averageVector(newPositions).sub(destination);
+        newPositions.map(newPosition => newPosition.sub(offsetFromDestination));
+
+        // Rotate the units in the direction they were moving.
+        const directionalAngle = destination.clone().sub(startingPoint).angle() + (Math.PI / 2);
+        newPositions = newPositions.map(newPosition => rotateAroundOrigin(destination, newPosition, directionalAngle));
+
+        // Find the shortest distance between each source and destination point in the formation.
+        const indexMap: {[key: number]: number} = {};
+        const newPositionIndexes = Array.from(Array(newPositions.length).keys());
+        const shortestPositions = Array();
+        positions.forEach((position, positionIndex) => {
+            const candidates = newPositionIndexes.filter(candidate => typeof indexMap[candidate] === 'undefined');
+            const distances = candidates.map(candidate => newPositions[candidate].distanceTo(position));
+            const shortestDistanceIndex = candidates[distances.indexOf(Math.min(...distances))];
+            shortestPositions[positionIndex] = newPositions[shortestDistanceIndex];
+            indexMap[shortestDistanceIndex] = positionIndex;
+        });
+
+        console.log(indexMap);
 
 
-        const directionalAngle = destination.clone().sub(startingPoint).angle();
 
 
 
 
 
-        // https://stackoverflow.com/a/6645412
-        circle(window.ctx, destination)
 
-
-        return linedUpDestinations.map(newPosition => [newPosition]);
+        return shortestPositions.map(newPosition => [newPosition]);
     }
 
 }
