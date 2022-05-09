e14e49e2fc94784839231213b3f6bcb2ff6c5ca0
Sam <54m152@gmail.com>
2022-05-09 10:58:33 +0800

    Log with kick

--- dist-server/kick.sh
+++ dist-server/kick.sh
@@ -2,4 +2,4 @@
 
 set +e
 killall node
-HOSTNAME="micromania.cc" KEY_FILE="/etc/letsencrypt/live/micromania.cc/privkey.pem" CERT_FILE="/etc/letsencrypt/live/micromania.cc/cert.pem" node bundle.js
+HOSTNAME="micromania.cc" KEY_FILE="/etc/letsencrypt/live/micromania.cc/privkey.pem" CERT_FILE="/etc/letsencrypt/live/micromania.cc/cert.pem" node bundle.js > /tmp/log.txt
