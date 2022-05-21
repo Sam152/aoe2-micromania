64973ea36c75cf88956a888d5f64d75ab8638b77
Sam <54m152@gmail.com>
2022-05-21 15:37:38 +0800

    Note

--- src/common/util/addWithClamp.ts
+++ src/common/util/addWithClamp.ts
@@ -2,6 +2,9 @@ import {Vector2} from 'three/src/math/Vector2';
 import Grid from '../terrain/Grid';
 
 export default function addWithClamp(position: Vector2, addition: Vector2, grid: Grid) {
+    /**
+     * Usually an isometric game will use the diamond layout exclusively for display. All world & gameplay logic is done in a separate coordinate system, with the axes aligned to the rows & columns (so eg. (2, 0) is the third column of the first row, assuming 0-based indices), and transformed to the isometric perspective only for the drawing step. This kind of switch will likely make a lot of your logic much simpler. –
+     */
     position.add(addition);
 }
 
