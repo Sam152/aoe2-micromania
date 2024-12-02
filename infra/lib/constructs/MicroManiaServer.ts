fbc5919f84c86997fdc333b4eeb226536d90cf96
Sam <54m152@gmail.com>
2024-12-02 20:21:10 +0800

    infra

--- infra/lib/constructs/MicroManiaServer.ts
+++ infra/lib/constructs/MicroManiaServer.ts
@@ -1,6 +1,5 @@
 import { CfnOutput, Tags } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as path from "path";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import {
   AmazonLinux2023ImageSsmParameter,
@@ -15,55 +14,50 @@ import {
   UserData,
   Vpc,
 } from "aws-cdk-lib/aws-ec2";
-import { DockerImageAsset, Platform } from "aws-cdk-lib/aws-ecr-assets";
+import { DockerImageAsset } from "aws-cdk-lib/aws-ecr-assets";
 import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 import { ManagedPolicy, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 import { Distribution, OriginProtocolPolicy, ViewerProtocolPolicy } from "aws-cdk-lib/aws-cloudfront";
 import { HttpOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
 
 type ServerProps = {
-  region: string; // region: "ap-southeast-2",
-  certArn: string; // Click ops only
-  apiDomain: "foo.example.com";
-  apiKeypairName: "mm-server";
+  region: string;
+  certArn: string;
+  apiDomain: string;
+  apiKeypairName: string;
   account: string;
+  container: DockerImageAsset;
 };
 
-export class Server extends Construct {
-  private readonly id: string;
+export class MicroManiaServer extends Construct {
   private props: ServerProps;
-  private image: DockerImageAsset;
 
   constructor(scope: Construct, id: string, props: ServerProps) {
     super(scope, id);
-    this.id = id;
     this.props = props;
-
-    this.buildContainer();
 
     // Deploy straight to EC2 as opposed to some container orchestration
     // system, because we require a single stateful server.
     this.deployContainerEc2();
 
-    Tags.of(this).add("CostCenter", "Server");
+    Tags.of(this).add("CostCenter", `Server-${id}`);
   }
 
   deployContainerEc2() {
     const vpc = Vpc.fromLookup(this, "default-vpc", { isDefault: true });
-    const securityGroup = new SecurityGroup(this, "mm-server-security-group", {
+    const securityGroup = new SecurityGroup(this, "sec-group", {
       vpc: vpc,
     });
     securityGroup.addIngressRule(Peer.anyIpv4(), Port.allTraffic());
     securityGroup.addEgressRule(Peer.anyIpv4(), Port.allTraffic());
 
-    const instanceRole = new Role(this, "mm-server-instance-role", {
+    const instanceRole = new Role(this, "instance-role", {
       assumedBy: new ServicePrincipal("ec2.amazonaws.com"),
     });
-    instanceRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AmazonDynamoDBFullAccess"));
     instanceRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ContainerRegistryReadOnly"));
 
-    const instance = new ec2.Instance(this, "mm-server-instance", {
-      keyPair: KeyPair.fromKeyPairName(this, "tp-prod-keypair", this.props.apiKeypairName),
+    const instance = new ec2.Instance(this, "instance", {
+      keyPair: KeyPair.fromKeyPairName(this, "key-pair", this.props.apiKeypairName),
       instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.NANO),
       vpc: vpc,
       machineImage: new AmazonLinux2023ImageSsmParameter(),
@@ -74,16 +68,16 @@ export class Server extends Construct {
       userData: this.buildInstanceUserData(),
       role: instanceRole,
       blockDevices: [
-        {
-          deviceName: "/dev/xvda",
-          volume: ec2.BlockDeviceVolume.ebs(8),
-        },
+        // {
+        //   deviceName: "/dev/xvda",
+        //   volume: ec2.BlockDeviceVolume.ebs(8),
+        // },
       ],
     });
 
-    new Distribution(this, `mm-server-api-distribution`, {
+    new Distribution(this, `distro`, {
       domainNames: [this.props.apiDomain],
-      certificate: Certificate.fromCertificateArn(this, "tp-api-cert", this.props.certArn),
+      certificate: Certificate.fromCertificateArn(this, "cert", this.props.certArn),
       defaultBehavior: {
         origin: new HttpOrigin(instance.instancePublicDnsName, {
           protocolPolicy: OriginProtocolPolicy.HTTP_ONLY,
@@ -92,7 +86,7 @@ export class Server extends Construct {
       },
     });
 
-    new CfnOutput(this, "mm-server-kick-script", {
+    new CfnOutput(this, "kick", {
       value: `ssh -i ~/.ssh/tp-prod.pem ec2-user@${instance.instancePublicDnsName} sudo /var/lib/cloud/instance/scripts/part-001`,
     });
   }
@@ -107,17 +101,9 @@ export class Server extends Construct {
       `systemctl start docker`,
       `aws ecr get-login-password --region ${this.props.region} | docker login --username AWS --password-stdin ${this.props.account}.dkr.ecr.${this.props.region}.amazonaws.com`,
       `docker system prune -a -f`,
-      `docker pull ${this.image.imageUri}`,
-      `docker container run -d -e --publish 80:80 --restart always ${this.image.imageUri}`,
+      `docker pull ${this.props.container.imageUri}`,
+      `docker container run -d -e --publish 80:80 --restart always ${this.props.container.imageUri}`,
     );
     return data;
-  }
-
-  buildContainer() {
-    this.image = new DockerImageAsset(this, "mm-server-image", {
-      directory: path.resolve(__dirname, "../../../"),
-      file: "server.Dockerfile",
-      platform: Platform.LINUX_AMD64,
-    });
   }
 }
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { Server } from "./constructs/Server";
+import { MicroManiaServer } from "./constructs/MicroManiaServer";
+import { DockerImageAsset, Platform } from "aws-cdk-lib/aws-ecr-assets";
+import * as path from "path";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,8 +10,20 @@ export class InfraStack extends cdk.Stack {
 
     // Deploy the client.
     // @todo
+    // client cert arn: arn:aws:acm:us-east-2:390772177583:certificate/9553d752-255a-467a-87c7-1d4b76fa2c53
 
-    // Add as many regional servers as we'd like to support.
-    new Server(this, "us-east-1-server", {});
+    // Create as many regional servers as required.
+    new MicroManiaServer(this, "us-east", {
+      account: "390772177583",
+      apiDomain: "us-east.aoe.cx",
+      region: "us-east-2",
+      certArn: "arn:aws:acm:us-east-2:390772177583:certificate/0f4459c1-7dcd-42ec-aeaf-6795c4472840",
+      apiKeypairName: "aoe-cx",
+      container: new DockerImageAsset(this, "server-container", {
+        directory: path.resolve(__dirname, "../../"),
+        file: "server.Dockerfile",
+        platform: Platform.LINUX_AMD64,
+      }),
+    });
   }
 }
