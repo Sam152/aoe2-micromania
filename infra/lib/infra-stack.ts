813c75297f646fd8f5e9f3357f7bccfab4c5761c
Sam <54m152@gmail.com>
2024-12-03 19:31:09 +0800

    Progress

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -6,7 +6,7 @@ import { DockerImageAsset, Platform } from "aws-cdk-lib/aws-ecr-assets";
 import * as path from "path";
 
 export class InfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
     // Deploy the client.
@@ -17,19 +17,30 @@ export class InfraStack extends cdk.Stack {
       socketHost: "https://us-east.aoe.cx",
     });
 
-    // Create as many regional servers as required.
+    const serverContainer = new DockerImageAsset(this, "server-container", {
+      directory: path.resolve(__dirname, "../../"),
+      file: "server.Dockerfile",
+      platform: Platform.LINUX_AMD64,
+    });
+
+    // Regional servers.
     new MicroManiaServer(this, "us-east", {
       account: "390772177583",
-      apiDomain: "us-east.aoe.cx",
       zoneId: "Z08280773DHMHUR6KDGGL",
+      apiDomain: "us-east.aoe.cx",
       region: "us-east-2",
       certArn: "arn:aws:acm:us-east-1:390772177583:certificate/6392c94f-f8f0-4e74-82dd-d1b9d9bed683",
       apiKeypairName: "aoe-cx",
-      container: new DockerImageAsset(this, "server-container", {
-        directory: path.resolve(__dirname, "../../"),
-        file: "server.Dockerfile",
-        platform: Platform.LINUX_AMD64,
-      }),
+      container: serverContainer,
+    });
+    new MicroManiaServer(this, "us-east", {
+      account: "390772177583",
+      zoneId: "Z08280773DHMHUR6KDGGL",
+      apiDomain: "ap-south.aoe.cx",
+      region: "ap-southeast-2",
+      certArn: "arn:aws:acm:us-east-1:390772177583:certificate/4ec1d899-34f3-4146-a155-fd7462d904cc",
+      apiKeypairName: "aoe-cx",
+      container: serverContainer,
     });
   }
 }
