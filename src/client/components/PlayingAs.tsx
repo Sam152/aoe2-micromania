925e379d1950a29f9461fa19d6e15d04070f78f7
Sam <54m152@gmail.com>
2024-12-07 22:22:39 +0800

    Progress

--- src/client/components/PlayingAs.tsx
+++ src/client/components/PlayingAs.tsx
@@ -1,4 +1,6 @@
 import { ConnectedState } from "../hooks/useConnectedState";
+import { Text } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 export function PlayingAs({ connectedState }: { connectedState: ConnectedState }) {
   const queued = connectedState.queuedPlayers.includes(connectedState.clientId);
@@ -7,15 +9,23 @@ export function PlayingAs({ connectedState }: { connectedState: ConnectedState }
     const positionInQueue = connectedState.queuedPlayers.findIndex((item) => item === connectedState.clientId) + 1;
 
     return (
-      <>
+      <Label>
         Queued {positionInQueue} out of {connectedState.queuedPlayers.length}
-      </>
+      </Label>
     );
   }
 
   if (connectedState.activePlayers[connectedState.clientId]) {
-    return <>Playing as {connectedState.activePlayers[connectedState.clientId]}</>;
+    return <Label>Playing as {connectedState.activePlayers[connectedState.clientId]}</Label>;
   }
 
   return null;
+}
+
+function Label({ children }: { children: ReactNode }) {
+  return (
+    <Text textTransform="uppercase" fontWeight="bold" fontSize="sm">
+      {children}
+    </Text>
+  );
 }
