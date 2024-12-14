8b3ae7c72f74bcc666443ed2e11e14486f09f152
Sam Becker <820219+Sam152@users.noreply.github.com>
2024-12-14 12:58:25 +1100

    Progress

--- src/common/units/formations/FormationBase.ts
+++ src/common/units/formations/FormationBase.ts
@@ -1,5 +1,6 @@
 import { FormationInterface, UnitInstance } from "../../../types";
 import { Vector2 } from "three/src/math/Vector2";
+import unitMetadataFactory from "../unitMetadataFactory";
 
 export default abstract class FormationBase implements FormationInterface {
   form(units: UnitInstance[], destination: Vector2): Array<Vector2> {
@@ -9,6 +10,12 @@ export default abstract class FormationBase implements FormationInterface {
     if (units.length === 1) {
       return [destination.clone()];
     }
+
+    const slowest = Math.min(...units.map((unit) => unitMetadataFactory.getUnit(unit.unitType).movementRate));
+    units.forEach((unit) => {
+      unit.groupMovementSpeed = slowest;
+    });
+
     return this.doForm(units, destination);
   }
 
