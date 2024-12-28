da9ccb1503c982531094d61061775d76b610bb51
Sam <54m152@gmail.com>
2024-12-28 15:05:21 +0800

    Progress

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,23 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/server/index.ts',
+  entry: "./src/server/index.ts",
   target: "node",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  optimization: {
+    minimize: false,
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist-server'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist-server"),
   },
   watchOptions: {},
 };
