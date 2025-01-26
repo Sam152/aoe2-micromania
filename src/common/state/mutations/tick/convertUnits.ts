f61c103327951ed39dca6fabfe0f9e0d3b979916
Sam <54m152@gmail.com>
2025-01-26 11:35:04 +0800

    Progress

--- src/common/state/mutations/tick/convertUnits.ts
+++ src/common/state/mutations/tick/convertUnits.ts
@@ -13,51 +13,52 @@ export default function convertUnits(state: GameState, computed: ComputedFrameSt
   const convertingUnits = state.units.filter((unit) => !!unit.convertingUnit);
 
   // Check if a unit should be firing or moving towards its target.
-  convertingUnits
-    // .filter((unit) => unit.unitState !== UnitState.Firing)
-    .forEach((monk) => {
-      const convertingUnit = computed.unitIndex[monk.convertingUnit];
-      if (!convertingUnit) {
-        monk.convertingUnit = undefined;
-        return;
+  convertingUnits.forEach((monk) => {
+    const convertingUnit = computed.unitIndex[monk.convertingUnit];
+
+    // Either the target of a conversion was already converted, or it cycled out
+    // of the unit list.
+    if (!convertingUnit || convertingUnit.ownedByPlayer === monk.ownedByPlayer) {
+      stopUnit(monk);
+      return;
+    }
+
+    const targetingPosition = convertingUnit.position;
+
+    if (inAttackRange(monk, targetingPosition)) {
+      monk.movingDirection = null;
+      monk.unitState = UnitState.Firing;
+      monk.direction = compassDirectionCalculator.getDirection(monk.position, targetingPosition);
+
+      if (!monk.conversionSucceedsAt) {
+        monk.conversionSucceedsAt = state.ticks + lockConversionSuccessInNumberTicks(state, monk);
+        monk.unitStateStartedAt = state.ticks;
       }
 
-      const targetingPosition = convertingUnit.position;
+      if (state.ticks === monk.conversionSucceedsAt) {
+        const converted = computed.unitIndex[monk.convertingUnit];
 
-      if (inAttackRange(monk, targetingPosition)) {
-        monk.movingDirection = null;
-        monk.unitState = UnitState.Firing;
-        monk.direction = compassDirectionCalculator.getDirection(monk.position, targetingPosition);
+        if (converted) {
+          converted.ownedByPlayer = monk.ownedByPlayer;
+          // Give the converted unit a new ID, so existing targeting and client side
+          // selections are lost.
+          converted.id = state.idAt++;
+          stopUnit(converted);
 
-        if (!monk.conversionSucceedsAt) {
-          monk.conversionSucceedsAt = state.ticks + lockConversionSuccessInNumberTicks(state, monk);
-          monk.unitStateStartedAt = state.ticks;
+          monk.conversionJuiceFullAt = state.ticks + CONVERSION_JUICE_TICKS_RECHARGE;
         }
-
-        if (state.ticks === monk.conversionSucceedsAt) {
-          const converted = computed.unitIndex[monk.convertingUnit];
-
-          if (converted) {
-            converted.ownedByPlayer = monk.ownedByPlayer;
-            // Give the converted unit a new ID, so existing targeting and client side
-            // selections are lost.
-            converted.id = state.idAt++;
-            stopUnit(converted);
-
-            monk.conversionJuiceFullAt = state.ticks + CONVERSION_JUICE_TICKS_RECHARGE;
-          }
-          stopUnit(monk);
-        }
-      } else {
-        setUnitMovementTowards(state, monk, targetingPosition);
-        monk.position.add(calculateUnitMovementPerTick(monk));
-
-        // When out of range, do not count towards a conversion tick.
-        if (monk.conversionSucceedsAt) {
-          monk.conversionSucceedsAt++;
-        }
+        stopUnit(monk);
       }
-    });
+    } else {
+      setUnitMovementTowards(state, monk, targetingPosition);
+      monk.position.add(calculateUnitMovementPerTick(monk));
+
+      // When out of range, do not count towards a conversion tick.
+      if (monk.conversionSucceedsAt) {
+        monk.conversionSucceedsAt++;
+      }
+    }
+  });
 }
 
 function lockConversionSuccessInNumberTicks(state: GameState, monk: UnitInstance) {
