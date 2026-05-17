4e9e5acd039369a07f0a07743ec902c320f16f07
Sam <54m152@gmail.com>
2026-05-17 19:48:47 +0800

    Progress

--- src/common/state/mutations/initiated/stopUnit.ts
+++ src/common/state/mutations/initiated/stopUnit.ts
@@ -3,28 +3,28 @@ import { UnitInstance } from "../../../../types.d.ts";
 
 export default function stopUnit(unit: UnitInstance): void {
   unit.unitState = UnitState.Idle;
-  unit.movingDirection = null;
+  unit.movingDirection = undefined;
   unit.waypoints = [];
   unit.clickedWaypoints = [];
-  unit.targetingPosition = null;
-  unit.targetingUnit = null;
-  unit.convertingUnit = null;
-  unit.conversionSucceedsAt = null;
-  unit.patrollingTo = null;
-  unit.patrollingToReturn = null;
-  unit.reformingTo = null;
-  unit.reformingSpeedFactor = null;
-  unit.reformingArrivalTick = null;
+  unit.targetingPosition = undefined;
+  unit.targetingUnit = undefined;
+  unit.convertingUnit = undefined;
+  unit.conversionSucceedsAt = undefined;
+  unit.patrollingTo = undefined;
+  unit.patrollingToReturn = undefined;
+  unit.reformingTo = undefined;
+  unit.reformingSpeedFactor = undefined;
+  unit.reformingArrivalTick = undefined;
 }
 
 export function stopUnitExceptForWaypoints(unit: UnitInstance): void {
   unit.unitState = UnitState.Idle;
-  unit.movingDirection = null;
-  unit.targetingPosition = null;
-  unit.targetingUnit = null;
-  unit.patrollingTo = null;
-  unit.patrollingToReturn = null;
-  unit.reformingTo = null;
-  unit.reformingSpeedFactor = null;
-  unit.reformingArrivalTick = null;
+  unit.movingDirection = undefined;
+  unit.targetingPosition = undefined;
+  unit.targetingUnit = undefined;
+  unit.patrollingTo = undefined;
+  unit.patrollingToReturn = undefined;
+  unit.reformingTo = undefined;
+  unit.reformingSpeedFactor = undefined;
+  unit.reformingArrivalTick = undefined;
 }
