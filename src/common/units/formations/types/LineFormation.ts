a79c2bfa3cb0d56cfabc9f1f784f29498652a38a
Sam <54m152@gmail.com>
2026-05-17 20:29:03 +0800

    Progress

--- src/common/units/formations/types/LineFormation.ts
+++ src/common/units/formations/types/LineFormation.ts
@@ -6,7 +6,7 @@ import { Vector2 } from "three/src/math/Vector2.js";
 export default class LineFormation extends FormationBase {
   distanceBetween = 25;
 
-  doForm(units: UnitInstance[], destination: Vector2): Array<Vector2> {
+  override doForm(units: UnitInstance[], destination: Vector2): Array<Vector2> {
     return formGroupedLines(units, destination, this.distanceBetween);
   }
 }
--- src/common/units/formations/types/SpreadFormation.ts
+++ src/common/units/formations/types/SpreadFormation.ts
@@ -6,7 +6,7 @@ import { formGroupedLines } from "../utilities/formGroupedLines.ts";
 export default class SpreadFormation extends FormationBase {
   distanceBetween = 40;
 
-  doForm(units: UnitInstance[], destination: Vector2): Array<Vector2> {
+  override doForm(units: UnitInstance[], destination: Vector2): Array<Vector2> {
     return formGroupedLines(units, destination, this.distanceBetween);
   }
 }
