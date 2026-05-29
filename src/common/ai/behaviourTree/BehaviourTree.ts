0aafe54da99840c4eeb63275030b96d74c24267b
Sam <54m152@gmail.com>
2026-05-29 22:14:52 +0800

    Progress

--- src/common/ai/behaviourTree/BehaviourTree.ts
+++ src/common/ai/behaviourTree/BehaviourTree.ts
@@ -20,3 +20,35 @@ type Sequence = {
 };
 
 export type BehaviourTree = Selector;
+
+const _sampleTree: BehaviourTree = {
+  nodeType: "selector",
+  nodes: [
+    {
+      nodeType: "sequence",
+      nodes: [
+        { nodeType: "condition", propertyName: "distanceToClosestOpponent", comparatorType: "GT", value: 1 },
+        {
+          nodeType: "action",
+          type: "PATROL",
+          params: {
+            direction: "TOWARDS",
+          },
+        },
+        {
+          nodeType: "selector",
+          nodes: [
+            { nodeType: "condition", propertyName: "distanceToClosestOpponent", comparatorType: "GT", value: 1 },
+            {
+              nodeType: "action",
+              type: "PATROL",
+              params: {
+                direction: "TOWARDS",
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
