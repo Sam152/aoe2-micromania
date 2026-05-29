3f74efd1ef1f4644095d4856d165e9fb4916074b
Sam <54m152@gmail.com>
2026-05-29 21:01:03 +0800

    Progress

--- src/common/ai/behaviourTree/blackboard/BlackboardValue.ts
+++
@@ -1,5 +0,0 @@
-import { DataType, TypeFromDataType } from "../dataType/dataTypes.ts";
-
-export type BlackboardValue<TDataType extends DataType> = TypeFromDataType<
-  TDataType
->;
--- src/common/ai/behaviourTree/blackboard/computeBlackboard.ts
+++ src/common/ai/behaviourTree/blackboard/computeBlackboard.ts
@@ -1,15 +1,31 @@
 import { GameState } from "../../../../types.ts";
 import { AgentState } from "../state/AgentState.ts";
-import { BlackboardValue } from "./BlackboardValue.ts";
+import { DataType, TypeFromDataType } from "../dataType/dataTypes.ts";
 
-export type Blackboard = {
-  distanceToClosestOpponent: BlackboardValue<"number">;
-  ticksSinceLastAction: BlackboardValue<"number">;
+export type BlackboardValueDefinition<TDataType extends DataType = DataType> = {
+  dataType: TDataType;
+  thing: TypeFromDataType<TDataType>;
+};
+
+type BlackboardDefinition = Record<string, BlackboardValueDefinition>;
+
+const blackboardDefinition = {
+  distanceToClosestOpponent: {
+    dataType: "number",
+    thing: 1,
+  },
+  ticksSinceLastAction: {
+    dataType: "number",
+  },
+} as const satisfies BlackboardDefinition;
+
+type BlackboardValuesFromDefinition<TBlackboard extends BlackboardDefinition> = {
+  [TKey: keyof TBlackboard]: TypeFromDataType<TBlackboard[TKey]["dataType"]>;
 };
 
 export function computeBlackboard(
   { gameState, agentState }: { gameState: GameState; agentState: AgentState },
-): Blackboard {
+): BlackboardValuesFromDefinition<typeof blackboardDefinition> {
   return {
     distanceToClosestOpponent: 100,
     ticksSinceLastAction: 100,
