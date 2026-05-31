88249a15b0ede2c3ffc57b7b5d879b0d4e0a37eb
Sam <54m152@gmail.com>
2026-05-31 22:10:38 +0800

    Progress

--- src/common/ai/behaviourTree/dataType/DataTypeDefinition.ts
+++ src/common/ai/behaviourTree/dataType/DataTypeDefinition.ts
@@ -1,3 +1,7 @@
+/**
+ * TDataType requires use in the body of this type for inference elsewhere
+ * to work.
+ */
 export type DataTypeDefinition<TDataType, TMutationRequirements> = {
   id: string;
   defaultValue: TDataType;
