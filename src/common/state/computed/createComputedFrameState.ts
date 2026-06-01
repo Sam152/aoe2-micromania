e5d52f0b42f4294609b8762ab8eb5010e43aefb6
Sam <54m152@gmail.com>
2026-06-01 20:58:04 +0800

    Progress

--- src/common/state/computed/createComputedFrameState.ts
+++ src/common/state/computed/createComputedFrameState.ts
@@ -4,6 +4,27 @@ import { createUnitQuadtree } from "../../util/buildQuadTree.ts";
 import { Grid } from "../../terrain/Grid.ts";
 import { memo } from "../../util/memo.ts";
 
+/**
+ * After every action, the state of the game changes. There are certain computation
+ * heavy things that can be computed as a function of the game state. Things like
+ * quad trees of unit positions.
+ *
+ * It may be that listeners require this computed state (like the AI for example needing
+ * the closest enemy unit), but it may also required by the subsequent action, like a
+ * tick auto firing on units.
+ *
+ * Thus the lifecycle of a tick is a little awkard in that we:
+ *
+ *  - Tick
+ *  - computedState = ...
+ *  - listeners(computedState)
+ *  - Tick(computedState)
+ *
+ *  ...and so on.
+ *
+ *  Each property is memoized, allowing us to have a lot of interesting computations,
+ *  which are computed a maximum of one time per changing game state.
+ */
 export type ComputedFrameState = {
   playerUnitQuadTrees: () => Record<number, Quadtree<UnitInstance>>;
   grid: () => Grid;
