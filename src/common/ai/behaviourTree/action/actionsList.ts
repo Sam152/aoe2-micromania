c92319b2400370037337f9f2784ca351c17334df
Sam <54m152@gmail.com>
2026-06-04 06:48:25 +0800

    Progress

--- src/common/ai/behaviourTree/action/actionsList.ts
+++ src/common/ai/behaviourTree/action/actionsList.ts
@@ -3,6 +3,7 @@ import { idle } from "./catalog/idle.ts";
 import { splitGroup } from "./catalog/splitGroup.ts";
 import { mergeGroup } from "./catalog/mergeGroup.ts";
 import { convert } from "./catalog/convert.ts";
+import { move } from "./catalog/move.ts";
 
 export const actionsList = {
   PATROL: patrol,
@@ -10,6 +11,7 @@ export const actionsList = {
   SPLIT_GROUP: splitGroup,
   MERGE_GROUP: mergeGroup,
   CONVERT: convert,
+  MOVE: move,
 } as const;
 
 export type ActionsList = typeof actionsList;
---
+++ src/common/ai/behaviourTree/action/catalog/move.ts
@@ -0,0 +1,23 @@
+import { defineAction } from "../ActionDefinition.ts";
+import { GameStateAction } from "../../../../../types.ts";
+import { Vector2 } from "three/src/math/Vector2.js";
+
+import { UnitType } from "../../../../units/UnitType.ts";
+
+export const move = defineAction({
+  type: "MOVE",
+  applicableForUnitType: [UnitType.Archer, UnitType.Mangonel, UnitType.Monk],
+  params: {
+    direction: {
+      dataType: "vector",
+      mutationRequirements: undefined,
+    },
+  },
+  execute: ({ direction }, { group }): GameStateAction => {
+    return {
+      n: "MOVE_UNITS_TO",
+      units: group.includedUnits,
+      position: new Vector2(direction.x, direction.y),
+    };
+  },
+});
