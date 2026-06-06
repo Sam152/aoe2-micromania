23712d1c25dce1c967e30df1ae839d748da67002
Sam <54m152@gmail.com>
2026-06-06 21:57:50 +0800

    Progress

--- src/common/ai/training/infra/repo/insertBot.ts
+++ src/common/ai/training/infra/repo/insertBot.ts
@@ -1,9 +1,10 @@
 import { sql } from "../connection.ts";
 import { UnitAwareBehaviourTree } from "../../../behaviourTree/BehaviourTree.ts";
-import { generateName } from "@criblinc/docker-names";
+import { generateName, left } from "@criblinc/docker-names";
+import { randomArray } from "../../../../util/randomArray.ts";
 
 export async function insertBot(tree: UnitAwareBehaviourTree): Promise<number> {
-  const botName = generateName();
+  const botName = `${randomArray(left)}-${generateName()}`;
   const [{ id }] = await sql`INSERT INTO bots (bot_name, tree) VALUES (${botName}, ${sql.json(tree)}) RETURNING id`;
   return id;
 }
