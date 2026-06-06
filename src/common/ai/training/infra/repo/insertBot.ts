84ba525e8b95127dab2222a03461004bcca3057f
Sam <54m152@gmail.com>
2026-06-06 16:16:54 +0800

    Progress

--- src/common/ai/training/infra/repo/insertBot.ts
+++ src/common/ai/training/infra/repo/insertBot.ts
@@ -1,7 +1,9 @@
+import { generateNameWithNumber } from "@criblinc/docker-names";
 import { sql } from "../connection.ts";
 import { UnitAwareBehaviourTree } from "../../../behaviourTree/BehaviourTree.ts";
 
 export async function insertBot(tree: UnitAwareBehaviourTree): Promise<number> {
-  const [{ id }] = await sql`INSERT INTO bots (tree) VALUES (${sql.json(tree)}) RETURNING id`;
+  const botName = generateNameWithNumber();
+  const [{ id }] = await sql`INSERT INTO bots (bot_name, tree) VALUES (${botName}, ${sql.json(tree)}) RETURNING id`;
   return id;
 }
--- src/common/ai/training/scripts/startTournamentHarness.ts
+++ src/common/ai/training/scripts/startTournamentHarness.ts
@@ -1,2 +1,15 @@
-export async function startTournamentHarness() {
+import { botsCount } from "../infra/repo/botsCount.ts";
+import { insertBot } from "../infra/repo/insertBot.ts";
+import { randomlyMutateUnitAwareBehaviourTree } from "../../mutation/randomlyMutateUnitAwareBehaviourTree.ts";
+import { sampleTree } from "../../behaviourTree/__fixtures__/sampleTree.ts";
+import { sql } from "../infra/connection.ts";
+
+async function startTournamentHarness() {
+  if (await botsCount() < 500) {
+    for (let i = 0; i < 500; i++) {
+      await insertBot(randomlyMutateUnitAwareBehaviourTree({ tree: sampleTree, count: 50 }));
+    }
+  }
 }
+
+startTournamentHarness().then(() => sql.end());
