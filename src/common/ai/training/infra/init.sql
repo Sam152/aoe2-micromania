260a13874218d8b4343516175fb385aeb9726470
Sam <54m152@gmail.com>
2026-06-06 15:28:45 +0800

    Progress

--- src/common/ai/training/infra/init.sql
+++ src/common/ai/training/infra/init.sql
@@ -1,15 +1,17 @@
-CREATE TABLE tree (
-  id SERIAL PRIMARY KEY,
+CREATE TABLE bots (
+  id BIGSERIAL PRIMARY KEY,
   tree JSONB NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+  elo INT NOT NULL DEFAULT 1600,
+  total_matches INT NOT NULL DEFAULT 0
 );
 
-CREATE TABLE game_result (
-  id SERIAL PRIMARY KEY,
-  player1_tree_id INTEGER NOT NULL REFERENCES tree(id),
-  player2_tree_id INTEGER NOT NULL REFERENCES tree(id),
-  player1_hp INTEGER NOT NULL,
-  player2_hp INTEGER NOT NULL,
-  ticks INTEGER NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+CREATE TABLE games (
+  id BIGSERIAL PRIMARY KEY,
+  player_1 BIGINT NOT NULL,
+  player_2 BIGINT NOT NULL,
+  actions JSONB NOT NULL,
+  final_state JSONB NOT NULL,
+  player_1_hp INT NOT NULL,
+  player_2_hp INT NOT NULL,
+  tick_count INT NOT NULL
 );
