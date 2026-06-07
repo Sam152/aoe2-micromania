8bb7c97636fe503a500a3d09dddfcdb4143d57ec
Sam <54m152@gmail.com>
2026-06-07 13:28:53 +0800

    Progress

--- src/common/ai/training/infra/repo/setLowestRatedPlayersToInactive.ts
+++ src/common/ai/training/infra/repo/setLowestRatedPlayersToInactive.ts
@@ -2,9 +2,15 @@ import { sql } from "../connection.ts";
 
 export async function setLowestRatedPlayersToInactive(count: number): Promise<void> {
   await sql`
-    UPDATE bots SET is_active = FALSE
-    WHERE id IN (
-      SELECT id FROM bots WHERE is_active = TRUE ORDER BY elo ASC LIMIT ${count}
-    )
+    UPDATE bots
+    SET is_active = FALSE
+    WHERE id IN (SELECT id
+                 FROM bots
+                 WHERE
+                   -- Always keep the seed bot for old times sake, see how it performs
+                   id != 1
+                   AND is_active = TRUE
+                 ORDER BY elo ASC
+                 LIMIT ${count})
   `;
 }
