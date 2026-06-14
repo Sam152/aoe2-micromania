2cb7f15c6a1a4fdc7c2854e81150ddf4fa0d63c4
Sam <54m152@gmail.com>
2026-06-14 13:42:50 +0800

    Progress

--- src/common/ai/training/infra/repo/getFewestNumberOfGamesPlayedByActiveBots.ts
+++
@@ -1,8 +0,0 @@
-import { sql } from "../connection.ts";
-
-export async function getFewestNumberOfGamesPlayedByActiveBots(): Promise<number> {
-  const [{ min }] = await sql<[{ min: number | null }]>`
-    SELECT MIN(wins + losses + draws) as min FROM bots WHERE is_active = TRUE
-  `;
-  return min ?? 0;
-}
