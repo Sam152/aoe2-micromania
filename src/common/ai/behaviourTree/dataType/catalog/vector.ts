0123e90f5f62a1dcf10f759585e396cae6452c4a
Sam <54m152@gmail.com>
2026-06-20 21:29:04 +0800

    Progress

--- src/common/ai/behaviourTree/dataType/catalog/vector.ts
+++ src/common/ai/behaviourTree/dataType/catalog/vector.ts
@@ -3,6 +3,10 @@ import { DataTypeDefinition } from "../DataTypeDefinition.ts";
 type Vector = { x: number; y: number };
 
 export const vector: DataTypeDefinition<Vector> = {
-  allowedValueTypes: ["BLACKBOARD", "LITERAL"],
+  /**
+   * Does any part of our system think a literal vector is a good idea?
+   * None that I can think of.
+   */
+  allowedValueTypes: ["BLACKBOARD"],
   defaultValue: { x: 0, y: 0 },
 };
