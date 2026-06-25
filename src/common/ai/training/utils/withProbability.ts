814f474eb92efa74466a20f4b212cec1089c3c8c
Sam <54m152@gmail.com>
2026-06-25 18:43:43 +0800

    Util

---
+++ src/common/ai/training/utils/withProbability.ts
@@ -0,0 +1,18 @@
+type Probabilities<T> = Array<{
+  probability: number;
+  effect: () => T;
+}>;
+
+export function withProbability<T>(probabilities: Probabilities<T>): T {
+  const total = probabilities.reduce((sum, { probability }) => sum + probability, 0);
+
+  let roll = Math.random() * total;
+  for (const { probability, effect } of probabilities) {
+    roll -= probability;
+    if (roll < 0) {
+      return effect();
+    }
+  }
+
+  return probabilities[probabilities.length - 1].effect();
+}
