13767dd3e6e299bc2ef8ab8ffc9b039abd536381
Sam <Sam152@users.noreply.github.com>
2026-07-14 17:20:58 +0800

    Progress

--- src/common/ai/training/infra/repo/getAllBorrowBots.ts
+++ src/common/ai/training/infra/repo/getAllBorrowBots.ts
@@ -9,9 +9,13 @@ const { TRAIT_BORROWING_MINIMUM_NUMBER_GROUPS, TRAIT_BORROWING_HIGHEST_N_BOTS_TO
 export async function getAllBorrowBots(): Promise<Bot[]> {
   const rows = await sql`
     SELECT *
-    FROM bots
-    WHERE is_active = false
-      AND (SELECT COUNT(DISTINCT group_name) FROM bots) >= ${TRAIT_BORROWING_MINIMUM_NUMBER_GROUPS}
+    FROM (
+      SELECT DISTINCT ON (group_name) *
+      FROM bots
+      WHERE is_active = false
+      ORDER BY group_name, elo DESC
+    ) highest_per_group
+    WHERE (SELECT COUNT(DISTINCT group_name) FROM bots) >= ${TRAIT_BORROWING_MINIMUM_NUMBER_GROUPS}
     ORDER BY elo DESC
     LIMIT ${TRAIT_BORROWING_HIGHEST_N_BOTS_TO_BORROW_FROM}`;
   return rows.map(botRowToBot);
